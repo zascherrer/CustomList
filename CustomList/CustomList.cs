893db8eedb29b46385538b38fddce3b078c7b718
Zachary Scherrer <zachary.a.scherrer@gmail.com>
2018-07-03 11:14:34 -0500

    Added seven unit tests for the Add method.

---
+++ CustomList/CustomList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomList
+{
+    class CustomList<T>
+    {
+        T[] array;
+        int numberOfSlots;
+
+        public CustomList()
+        {
+            numberOfSlots = 0;
+            array = new T[numberOfSlots];
+        }
+
+
+    }
+}
--- CustomListTests/AddMethodTests.cs
+++ CustomListTests/AddMethodTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomList;
 
 namespace CustomListTests
 {
@@ -7,9 +8,116 @@ namespace CustomListTests
     public class AddMethodTests
     {
         [TestMethod]
-        public void Add_SingleVariable_ReturnsSameVariable()
+        public void AddSingleVariable_CheckIndex0_ReturnsSameVariable()
         {
+            //Arrange
+            CustomList<int> myList = new CustomList<int>();
+            int value = 40;
 
+            //Act
+            myList.Add(40);
+
+            //Assert
+            Assert.AreEqual(value, myList[0]);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void AddSingleVariable_CheckIndex1_ThrowsException()
+        {
+            //Arrange
+            CustomList<int> myList = new CustomList<int>();
+            int value = 40;
+
+            //Act
+            myList.Add(40);
+            myList[1];
+
+            //Assert
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void AddSingleVariable_CheckIndexNegative1_ThrowsException()
+        {
+            //Arrange
+            CustomList<int> myList = new CustomList<int>();
+            int value = 40;
+
+            //Act
+            myList.Add(40);
+            myList[-1];
+
+            //Assert
+        }
+
+        [TestMethod]
+        public void AddTenVariables_CheckIndex0_ReturnsSameVariable()
+        {
+            //Arrange
+            CustomList<int> myList = new CustomList<int>();
+            int[] values = new int[10];
+
+            //Act
+            for(int i = 0; i < 10; i++)
+            {
+                values[i] = i;
+                myList.Add(values[i]);
+            }
+
+            //Assert
+            Assert.AreEqual(values[0], myList[0]);
+        }
+
+        [TestMethod]
+        public void AddTenVariables_CheckIndex5_ReturnsSameVariable()
+        {
+            //Arrange
+            CustomList<int> myList = new CustomList<int>();
+            int[] values = new int[10];
+
+            //Act
+            for (int i = 0; i < 10; i++)
+            {
+                values[i] = i;
+                myList.Add(values[i]);
+            }
+
+            //Assert
+            Assert.AreEqual(values[5], myList[5]);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void AddTenVariables_CheckIndex10_ThrowsException()
+        {
+            //Arrange
+            CustomList<int> myList = new CustomList<int>();
+            int[] values = new int[10];
+
+            //Act
+            for (int i = 0; i < 10; i++)
+            {
+                values[i] = i;
+                myList.Add(values[i]);
+            }
+
+            //Assert
+            Assert.AreEqual(values[9], myList[9]);
+        }
+
+        [TestMethod]
+        public void AddSingleVariable_CheckCount_Returns1()
+        {
+            //Arrange
+            CustomList<int> myList = new CustomList<int>();
+            int value = 40;
+
+            //Act
+            myList.Add(40);
+
+            //Assert
+            Assert.AreEqual(myList.Count, 1);
         }
     }
 }

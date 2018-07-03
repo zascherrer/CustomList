2def55c54fb97119d0dfbd2499420d1e8dde8d53
Zachary Scherrer <zachary.a.scherrer@gmail.com>
2018-07-03 10:38:54 -0500

    Initial

---
+++ CustomListTests/AddMethodTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CustomListTests
+{
+    [TestClass]
+    public class AddMethodTests
+    {
+        [TestMethod]
+        public void Add_SingleVariable_ReturnsSameVariable()
+        {
+
+        }
+    }
+}
